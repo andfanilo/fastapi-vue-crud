d578d42a1cf1eea26a08eeb152d950bf9c88b277
Fanilo Andrianasolo <andfanilo@gmail.com>
2019-11-26 18:30:55 +0100

    :sparkles: Implement get all books API endpoint

--- server/src/app.py
+++ server/src/app.py
@@ -6,6 +6,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+#: Configure CORS
 origins = [
     "http://localhost:8080",
 ]
@@ -19,18 +20,60 @@ app.add_middleware(
 )
 
 
-class Pong(BaseModel):
+#: Initialize list of books
+class Book(BaseModel):
+    title: str
+    author: str
+    read: bool
+
+
+BOOKS = [
+    Book(title="On the Road", author="Jack Kerouac", read=True),
+    Book(
+        title="Harry Potter and the Philosopher's Stone",
+        author="J. K. Rowling",
+        read=False,
+    ),
+    Book(title="Green Eggs and Ham", author="Dr. Seuss", read=True),
+]
+
+
+#: Describe all Pydantic Response classes
+class ResponseBase(BaseModel):
     status: str
     code: int
-    messages: List[str]
-    result: str
+    messages: List[str] = []
 
 
-@app.get("/ping", response_model=Pong)
-def ping_pong():
-    return {"status": "ok", "code": 200, "messages": [], "result": "Pong!"}
+class PongResponse(ResponseBase):
+    data: str
 
 
+class BooksResponse(ResponseBase):
+    data: List[Book]
+
+
+#: Mount routes
+@app.get("/")
+def index():
+    return {
+        "status": "ok",
+        "code": 200,
+        "data": "Welcome, please check /docs or /redoc",
+    }
+
+
+@app.get("/ping", response_model=PongResponse)
+def return_pong():
+    return {"status": "ok", "code": 200, "data": "Pong!"}
+
+
+@app.get("/books", response_model=BooksResponse)
+def get_all_books():
+    return {"status": "ok", "code": 200, "data": BOOKS}
+
+
+#: Start application
 if __name__ == "__main__":
     import uvicorn
 
--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -1,21 +1,36 @@
 from starlette.testclient import TestClient
 
-from server.src.app import app
+from server.src.app import app, Book, BOOKS
 
 client = TestClient(app)
 
 
 def test_index():
     response = client.get("")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Not Found"}
+    assert response.status_code == 200
+    data = response.json()
+    assert data["status"] == "ok"
+    assert data["code"] == 200
+    assert data["data"] == "Welcome, please check /docs or /redoc"
 
 
 def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
     data = response.json()
+    assert data["status"] == "ok"
     assert data["code"] == 200
     assert data["messages"] == []
-    assert data["result"] == "Pong!"
+    assert data["data"] == "Pong!"
+
+
+def test_get_all_books():
+    response = client.get("/books")
+    assert response.status_code == 200
+    data = response.json()
     assert data["status"] == "ok"
+    assert data["code"] == 200
+    assert data["messages"] == []
+
+    books = data["data"]
+    assert [Book(**book) for book in books] == BOOKS
